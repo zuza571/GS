758ab6c9804925bbce7210907f7dd64662aca090
zuza571 <zuzanna.pietrzak571@gmail.com>
2022-06-28 17:19:29 +0200

    initial commit

---
+++ src/main/java/com/example/games4u/Games4UApplication.java
@@ -0,0 +1,14 @@
+package com.example.games4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Games4UApplication {
+    // commit
+
+    public static void main(String[] args) {
+        SpringApplication.run(Games4UApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/games4u/Games4UApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.games4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Games4UApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
