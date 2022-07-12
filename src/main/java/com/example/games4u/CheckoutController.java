f8247f8519bc67a03b68024c5862ba441a243d68
Zuzanna Pietrzak <zuzanna.pietrzak571@gmail.com>
2022-07-12 16:26:34 +0200

    Add files via upload

--- src/main/java/com/example/games4u/CheckoutController.java
+++ src/main/java/com/example/games4u/CheckoutController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class CheckoutController {
 
     @RequestMapping("/checkout")
-    public String homePage(){
+    public String checkoutPage(){
         return "checkout.html";
     }
 }
