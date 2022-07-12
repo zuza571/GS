5944d1561f411b8d875fa1300f388c4738fb28de
zuza571 <zuzanna.pietrzak571@gmail.com>
2022-07-12 03:53:17 +0200

    home and contact view

--- src/main/java/com/example/games4u/CheckoutController.java
+++ src/main/java/com/example/games4u/CheckoutController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CheckoutController {
-
     @RequestMapping("/checkout")
     public String checkoutPage(){
         return "checkout.html";
