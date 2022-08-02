3d755f2236f77f71219d128b9d82d3dac795ec9c
zuza571 <zuzanna.pietrzak571@gmail.com>
2022-08-02 19:25:02 +0200

    game properties

--- src/main/java/com/example/games4u/CheckoutController.java
+++ src/main/java/com/example/games4u/CheckoutController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
 public class CheckoutController {
 
     @RequestMapping("/checkout")
