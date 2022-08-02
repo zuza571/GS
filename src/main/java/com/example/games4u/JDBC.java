8772f1b4029b96ee2fa5a1197d987a9752d6f0f0
JakubMazur965 <jakubmazur@student.agh.edu.pl>
2022-08-02 12:08:06 +0200

    JDBC

--- src/main/java/com/example/games4u/JDBC.java
+++ src/main/java/com/example/games4u/JDBC.java
@@ -3,25 +3,42 @@ import java.sql.*;
 
 public class JDBC {
 
-    static String dataFromDB;
+    static String daneZBazy;
+
+    static void wyswietlDaneZBazy(ResultSet rs){
+        try{
+            daneZBazy = rs.getString(1);
+            System.out.println("\n" + daneZBazy + " ");
+            daneZBazy = rs.getString(2);
+            System.out.println(daneZBazy + " ");
+            daneZBazy = rs.getString(3);
+            System.out.println(daneZBazy);
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
-        String connectionURL = "jdbc:mysql://localhost:3306/Games4UDataBase/test?user=root&password=zuzaikuba";
+        String connectionURL = "jdbc:mysql://localhost:3306/mydb?user=root&password=zuzaikuba";
         // Proste zapytanie do bazy danych
-        String query = "Select * FROM Games";
+        String query = "Select * FROM mydb.games";
 
         Connection conn = null;
 
         try {
+            // Ustawiamy sterownik MySQL - jest to generalnie niepotrzebne gdyż dzieje się automatycznie
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
             // Ustawiamy dane dotyczące połączenia
             conn = DriverManager.getConnection(connectionURL);
-
-            // Ustawiamy sterownik MySQL
-            Class.forName("com.mysql.jdbc.Driver");
 
             // Uruchamiamy zapytanie do bazy danych
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                wyswietlDaneZBazy(rs);
+            }
 
             conn.close();
         } catch (ClassNotFoundException e){
