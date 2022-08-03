6f79efe327b84b8c1516872d8cbf0bdd550e027b
JakubMazur965 <jakubmazur@student.agh.edu.pl>
2022-08-03 16:49:36 +0200

    DataBase

--- src/main/java/com/example/games4u/DataBase.java
+++ src/main/java/com/example/games4u/DataBase.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/*
 @Repository
 public interface DataBase extends JdbcRepositoriesAutoConfiguration<Game, Integer> {
     List<Game> games = new ArrayList<>();
 }
+*/
