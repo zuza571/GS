adc1c7710df41caeb8aee39cf8b43e055dc82e28
zuza571 <zuzanna.pietrzak571@gmail.com>
2022-08-03 20:39:24 +0200

    game properties

--- src/main/java/com/example/games4u/CheckoutController.java
+++ src/main/java/com/example/games4u/CheckoutController.java
@@ -1,13 +1,16 @@
 package com.example.games4u;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CheckoutController {
 
     @RequestMapping("/checkout")
-    public String checkoutPage(){
+    public String checkoutPage(Model model){
+        Game g1 = new Game(1, "Grand Theft Auto V", "Sandbox", 100);
+        model.addAttribute("g1", g1);
         return "checkout.html";
     }
 }
