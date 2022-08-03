748b5f6418014641350fda21452a4cddabbf688c
JakubMazur965 <jakubmazur@student.agh.edu.pl>
2022-08-03 18:55:18 +0200

    test

--- src/main/java/com/example/games4u/Games4UApplication.java
+++ src/main/java/com/example/games4u/Games4UApplication.java
@@ -1,26 +1,17 @@
 package com.example.games4u;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 @SpringBootApplication
 @Controller
 public class Games4UApplication {
-
-    // nwm czy to tu :(((((
-    /*
-    //@Autowired
-    //DataBase games;
-
-     */
 
     public static void main(String[] args) {
         SpringApplication.run(Games4UApplication.class, args);
