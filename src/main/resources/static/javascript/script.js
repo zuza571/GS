496e3c68caad073b302d53a2adc1a68c7b6a8acd
zuza571 <zuzanna.pietrzak571@gmail.com>
2022-08-07 11:59:27 +0200

    js cart + and - work

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -1,31 +1,61 @@
 
-$().ready(function() {
-    $('.minus').click(function () {
-        var parent = $(this).closest(".amount");
-        var input = parseInt(parent.find(".amount-input").val());
-        console.log(input);
-        var count = input - 1;
-        console.log(count);
 
-        // to zle dziala, ale w konsoli widac, ze dobrze zczytuje oba
-        // wpisuje w zle miejsce
-        //input['value'] = count;
-        document.querySelector("input").value = count;
+$().ready(function() {
+    let subtotal = 0;
+
+    $('.add-to-cart').click(function () {
+        const id = $(this).data('id');
+        alert("id gry do koszyka: " + id + ", tytuł: ");
+        // zrobic funkcje - jakies powiazania z baza danych
+        const title = getTitle(id);
+        const price = getPrice(id);
+        const image = getImage(id);
+    });
+
+
+    $('.minus').click(function () {
+        /*
+        var parent = $(this).parent().parent();
+        var input = parseInt(parent.find(".amount-input").val());
+        var count = input - 1;
+        //$(this).parent().parent().closest("input").value = count;
+        parent.closest("input.amount-input").value = count;
+         */
+
+        // + before $ changes value form String to int
+        if ($(this).next().val() > 1) {
+            let quantity = $(this).next().val(+$(this).next().val() - 1);
+
+            quantity = quantity.val();
+            let subtotal = document.querySelector("#items-price").textContent = (quantity * 100).toString() + ' PLN';
+            let total = document.querySelector("#items-price-total").textContent = ((quantity * 100) + 15).toString() + ' PLN';
+            console.log(quantity);
+            console.log(subtotal + ", " + total);
+        }
+
     });
 
     $('.plus').click(function () {
-        var parent = $(this).closest(".amount");
+        /*
+        var parent = $(this).parent().parent();
         var input = parseInt(parent.find(".amount-input").val());
-        console.log(input);
         var count = input + 1;
-        console.log(count);
+        parent.closest("input.amount-input").value = count;
+        */
 
-        //input['value'] = count;
-        document.querySelector("input").value = count;
+        let quantity = $(this).prev().val(+$(this).prev().val() + 1);
+
+        quantity = quantity.val();
+        let subtotal = document.querySelector("#items-price").textContent = (quantity * 100).toString() + ' PLN';
+        let total = document.querySelector("#items-price-total").textContent = ((quantity * 100) + 15).toString() + ' PLN';
+        console.log(quantity)
+        console.log(subtotal + ", " + total);
+
+
     });
 
-    $('#remove').click(function () {
-       $(this).parent().parent().parent().remove();
+    $('.remove-button').click(function () {
+        $(this).parent().parent().parent().remove();
     });
 
 });
