bd2b936374d5b97d7bff3c987e3b2e0e0bfeb989
JakubMazur965 <jakubmazur@student.agh.edu.pl>
2022-08-08 19:07:25 +0200

    session

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -2,21 +2,40 @@
 
 $().ready(function() {
     let subtotal = 0;
+    let listIds = [];
 
     $('.add-to-cart').click(function () {
-        let id;
+        let id = $(this).data('id');
         let add_buttons = document.querySelectorAll(".btn-area")
         for (const btn of add_buttons){
             btn.addEventListener("click", event => {
-                id = $(this).data('id');
-                let url = `http://localhost:8080/add/cart/${id}/`
-                console.log(url)
-                fetch(url)
-                    .then(response => console.log("Success") )
-                    .catch(error => {
-                        console.log(error);
-                    });
-
+                if (listIds.length === 0) {
+                    listIds.push(id)
+                    let url = `http://localhost:8080/add/cart/${id}/`
+                    console.log(url)
+                    fetch(url)
+                        .then(response => console.log("Success"))
+                        .catch(error => {
+                            console.log(error);
+                        });
+                } else {
+                    let helper = 0;
+                    for (let i = 0; i < listIds.length; i++) {
+                        if (listIds[i] === id) {
+                            helper = 1;
+                        }
+                    }
+                    if (helper === 0) {
+                        listIds.push(id)
+                        let url = `http://localhost:8080/add/cart/${id}/`
+                        console.log(url)
+                        fetch(url)
+                            .then(response => console.log("Success"))
+                            .catch(error => {
+                                console.log(error);
+                            });
+                    }
+                }
             })
         }
     });
